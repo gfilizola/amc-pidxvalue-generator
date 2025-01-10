a298b8eca8c8bfe7a19d5cbec876411425fa50f1
Gustavo Filizola Soares <gfilizolasoares@ippon.fr>
2025-01-10 16:30:26 +0100

    feat: AMC PIDXValue Generator

--- src/main/java/com/ippon/bm/tools/AMCPIDXValueGenerator.java
+++ src/main/java/com/ippon/bm/tools/AMCPIDXValueGenerator.java
@@ -2,13 +2,18 @@ package com.ippon.bm.tools;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import org.apache.commons.lang3.StringUtils;
 
 public class AMCPIDXValueGenerator {
 
-    public static String[] generatePIDs(SecretKey key, BigInteger V) throws Exception {
+    public static String[] generatePIDs(String keyHex, String vHex) throws Exception {
+        byte[] keyBytes = hexStringToByteArray(keyHex);
+        SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+
+        BigInteger V = new BigInteger(vHex, 16); // Example unique value
         String[] PIDs = new String[35];
 
         BigInteger PL = V.shiftRight(16);
@@ -36,6 +41,16 @@ public class AMCPIDXValueGenerator {
         BigInteger pid = currentPL.shiftLeft(16).add(currentPR);
         String pidHex = pid.toString(16).toUpperCase();
         return StringUtils.leftPad(pidHex, 8, '0');
+    }
+
+    private static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
     }
 
     private static BigInteger TDESencryption(SecretKey key, BigInteger data) throws Exception {
--- src/test/java/com/ippon/bm/tools/AMCPIDXValueGeneratorTest.java
+++ src/test/java/com/ippon/bm/tools/AMCPIDXValueGeneratorTest.java
@@ -1,10 +1,6 @@
 package com.ippon.bm.tools;
 
 import org.junit.jupiter.api.Test;
-
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.math.BigInteger;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,11 +10,8 @@ class AMCPIDXValueGeneratorTest {
     @Test
     void generatePIDs() throws Exception {
         String keyHex = "12345678876543219ABCDEF00FEDCBA91234567887654321";
-        byte[] keyBytes = hexStringToByteArray(keyHex);
-        SecretKey key = new SecretKeySpec(keyBytes, "DESede");
-
-        BigInteger V = new BigInteger("000ABC12", 16); // Example unique value
-        String[] PIDs = AMCPIDXValueGenerator.generatePIDs(key, V);
+        String vHex = "000ABC12";
+        String[] PIDs = AMCPIDXValueGenerator.generatePIDs(keyHex, vHex);
 
         System.out.println("Generated PIDs: " + Arrays.toString(PIDs));
         Arrays.stream(PIDs).forEach(System.out::println);
@@ -35,13 +28,4 @@ class AMCPIDXValueGeneratorTest {
         assertThat(PIDs[9]).isEqualTo("963458A2");
     }
 
-    private byte[] hexStringToByteArray(String s) {
-        int len = s.length();
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                    + Character.digit(s.charAt(i + 1), 16));
-        }
-        return data;
-    }
 }
